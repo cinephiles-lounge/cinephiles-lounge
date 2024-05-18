2c5051ea1cf89fcb45fa44bbe7ac46a1a81f3e89
garamgim <garamgim@gmail.com>
2024-05-18 11:29:39 +0900

    feat: 게시글 조회, 생성 기능 완료
    - Article 모델 변경 (마이그레이션 필요)
      - lounge 외래키 null=True 추가

--- back/articles/views.py
+++ back/articles/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+
+# 게시글 전체 목록 조회
+@api_view(['GET'])
+def get_article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK) 
+
+
+# 게시글 생성
+@api_view(['POST'])
+def create_article(request, movie_id):
+    if request.method == 'POST':
+        movie = Movie.objects.get(movie_id=movie_id)
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user, movie=movie)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
