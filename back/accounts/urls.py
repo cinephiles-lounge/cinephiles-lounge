8173068ead4902120f58966c1808cea19c9723c8
garamgim <garamgim@gmail.com>
2024-05-18 10:28:33 +0900

    feat: 유저가 좋아요 한 영화 조회 기능 완료

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('subscribe/<int:to_user_pk>/', views.subscribe),
+    path('liked_movies/', views.liked_movies),
 ]
--- back/accounts/views.py
+++ back/accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-
+from movies.serializers import MovieSerializer
 
 User = get_user_model()
 
@@ -19,4 +19,12 @@ def subscribe(request, to_user_pk):
             'subscriber_count': to_user.subscribers.count()
         }
         return Response(data, status=status.HTTP_200_OK)
+    
+
+@api_view(['GET'])
+def liked_movies(request):
+    if request.method == 'GET':
+        movies = request.user.liked_movies.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
