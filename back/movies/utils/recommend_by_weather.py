6d8bfee93608ef0bbd914cc4178d0831b801fbd4
garamgim <garamgim@gmail.com>
2024-05-21 13:27:23 +0900

    feat: 날씨 기반 AI 추천 기능 완성, Movie 모델 fixture 업데이트
    - 영화 정보 약 4000개 업데이트

--- back/movies/utils/recommend_by_weather.py
+++ back/movies/utils/recommend_by_weather.py
@@ -1,23 +1,49 @@
 from openai import OpenAI
 from django.conf import settings
+import requests
+from .load import save_movies
 
 def recommend_by_weather(weather_id):
     weather_message = ""
+    recommend_conditions = ""
     if weather_id < 300:
         # Thunderstorm (2XX)
         weather_message = "천둥 번개가 몰려와요"
+        recommend_conditions = """
+            1. Genres: Action, SF, Thriller, Horror
+            2. Fast paced story and loud music, dynamic scenes 
+        """ 
     elif weather_id < 600:
         # Drizzle (3XX), Rain (5XX)
         weather_message = "촉촉한 비가 내려요"
+        recommend_conditions = """
+            1. Genres: Mistery, Classic, Romance, Indie
+            2. Peaceful and tranquil, artistic
+            3. Beatiful music and scenes
+        """ 
     elif weather_id < 700:
         # Snow (6XX)
         weather_message = "하얀 눈이 내려요"
+        recommend_conditions = """
+            1. Genres: Romance, Family-Friendly
+            2. Happy and exciting story
+            3. Brings back childhood memories
+            4. Great for christmast and snowy winter
+        """ 
     elif weather_id == 800:
         # Clear (800)
         weather_message = "맑은 하늘이 아름다운 날이에요"
+        recommend_conditions = """
+            1. Genres: Feel-Good Comedies, Adventure, Drama
+            2. Cheerful and dramatic story
+        """ 
     else:
         # Atmosphere (8XX), Clouds (80X)
         weather_message = "우중충 하고 흐린 날이에요"
+        recommend_conditions = """
+            1. Genres: Mistery, Thriller, Detective, Noir
+            2. Dark and hard-boiled story
+        """ 
     
     client = OpenAI(
         api_key=settings.OPEN_API_KEY
@@ -28,9 +54,43 @@ def recommend_by_weather(weather_id):
         messages=[
             {
                 "role": "user",
-                "content": "화창한 날에 어울리는 가족 영화를 추천해줘",
+                "content": f"""
+                Please recommend 10 movies that matches beyond conditions
+
+                {recommend_conditions}
+
+                Don't put additional words on your message.
+                Just send me a list of movies in a format of a list.
+                This unordered list should not contain any special character, 
+                and movie titles should be seperated by '\n'.
+                You can't make up the fake movie names.
+                You only can recommend existing movies.
+                """,
             },
         ],
     )
 
-    print(completion.choices[0].message)
+    response = completion.choices[0].message.content
+    response = response.split('\n')
+
+    recommended_movies = []
+    recommended_movies_ids = []
+
+    for movie_title in response:
+        movie_title = movie_title[2:]
+        url = f"https://api.themoviedb.org/3/search/movie?query={movie_title}&include_adult=true&language=ko-KR&page=1"
+        response = requests.get(url, headers=headers).json().get('results')
+        if response:
+            result_movie = response[0]
+            recommended_movies.append(result_movie)
+            recommended_movies_ids.append(result_movie.get('id'))
+
+    return weather_message, recommended_movies_ids
+
+
+tmdb_access_token = settings.TMDB_ACCESS_TOKEN
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {tmdb_access_token}"        
+}
