cc0aca1e6a121eecb10acf2de408b4eac70c9af5
garamgim <garamgim@gmail.com>
2024-05-21 21:48:26 +0900

    fix: 모달 기능 수정
    - 여닫는 toggle 기능 후 폼 초기화
    - 에러 메시지 렌더링
    - 생성 후 getUserInfo 호출하여 화면 업데이트

--- front/cinephileslounge/src/stores/lounges.js
+++ front/cinephileslounge/src/stores/lounges.js
@@ -7,53 +7,7 @@ import { useRouter } from "vue-router";
 export const useLoungeStore = defineStore(
   "lounges",
   () => {
-    const accountStore = useAccountStore();
-
-    const joinLounge = function (codeInput) {
-      axios({
-        method: "post",
-        url: `${accountStore.API_URL}/lounges/join/`,
-        headers: {
-          Authorization: `Token ${accountStore.token}`,
-        },
-        data: {
-          code: codeInput.value,
-        },
-      })
-        .then((res) => {
-          console.log("라운지 가입 완료");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-
-    const createLounge = function (payload) {
-      const { loungeName, loungeDescription } = payload;
-
-      axios({
-        method: "post",
-        url: `${accountStore.API_URL}/lounges/create/`,
-        headers: {
-          Authorization: `Token ${accountStore.token}`,
-        },
-        data: {
-          name: loungeName,
-          description: loungeDescription,
-        },
-      })
-        .then((res) => {
-          console.log("라운지 생성 완료");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-
-    return {
-      joinLounge,
-      createLounge,
-    };
+    return {};
   },
   { persist: true }
 );
--- front/cinephileslounge/src/views/MyPageView.vue
+++ front/cinephileslounge/src/views/MyPageView.vue
@@ -53,18 +53,19 @@
         <!-- 여기 할 차례 -->
         <button
           id="join-lounge-button"
-          @click="switchMode('join'), openModal($event)"
+          @click="switchMode('join'), toggleModal($event)"
         >
           <i class="bx bx-network-chart"></i>
         </button>
         <button
           id="create-lounge-button"
-          @click="switchMode('create'), openModal($event)"
+          @click="switchMode('create'), toggleModal($event)"
         >
           <i class="bx bx-add-to-queue"></i>
         </button>
       </h3>
       <ul v-if="accountStore.joinedLounges" class="slide">
+        <!-- 내가 관리자인 라운지를 먼저 렌더링 -->
         <li v-for="lounge in accountStore.joinedLounges" :key="lounge.id">
           {{ lounge.name }}
         </li>
@@ -72,27 +73,26 @@
       <p v-else>아직 가입한 라운지가 없습니다.</p>
     </div>
 
-    <div class="modal-wrap" v-show="isModalOpen" @click="openModal">
+    <div class="modal-wrap" v-show="isModalOpen" @click="toggleModal">
       <div class="modal-container" @click.stop="">
         <template v-if="currentMode === 'join'">
-          <form @submit.prevent="loungeStore.joinLounge(codeInput)">
+          <form @submit.prevent="joinLounge">
             <div class="modal-input">
               <label for="code">가입 코드를 입력하세요.</label>
               <input type="text" id="code" v-model="codeInput" />
             </div>
+
+            <p v-show="errorMessage">{{ errorMessage }}</p>
+
             <div class="modal-btn">
-              <button type="submit">생성</button>
-              <button @click.prevent="openModal">닫기</button>
+              <button type="submit">가입</button>
+              <button @click.prevent="toggleModal">닫기</button>
             </div>
           </form>
         </template>
 
         <template v-if="currentMode === 'create'">
-          <form
-            @submit.prevent="
-              loungeStore.createLounge({ loungeName, loungeDescription })
-            "
-          >
+          <form @submit.prevent="createLounge">
             <div class="modal-input">
               <label for="name">라운지 이름</label>
               <input
@@ -112,9 +112,12 @@
                 required
               ></textarea>
             </div>
+
+            <p v-show="errorMessage">{{ errorMessage }}</p>
+
             <div class="modal-btn">
-              <button type="submit">가입</button>
-              <button @click.prevent="openModal">닫기</button>
+              <button type="submit">생성</button>
+              <button @click.prevent="toggleModal">닫기</button>
             </div>
           </form>
         </template>
@@ -129,6 +132,7 @@ import { useAccountStore } from "@/stores/account";
 import { useLoungeStore } from "@/stores/lounges";
 import { useRouter } from "vue-router";
 import FeedCard from "@/components/Feed/FeedCard.vue";
+import axios from "axios";
 
 const router = useRouter();
 const accountStore = useAccountStore();
@@ -144,8 +148,12 @@ const switchMode = function (status) {
 
 // 모달 여닫는 기능
 const isModalOpen = ref(false);
-const openModal = function () {
+const toggleModal = function () {
   isModalOpen.value = !isModalOpen.value;
+  codeInput.value = "";
+  loungeName.value = "";
+  loungeDescription.value = "";
+  errorMessage.value = "";
 };
 
 // 라운지 가입 - 코드 작성란 v-model
@@ -154,6 +162,50 @@ const codeInput = ref("");
 // 라운지 생성 - 이름, 소개 작성란 v-model
 const loungeName = ref("");
 const loungeDescription = ref("");
+
+// 가입, 생성 시 요청 에러가 나면 메시지를 띄운다
+const errorMessage = ref("");
+
+const joinLounge = function () {
+  axios({
+    method: "post",
+    url: `${accountStore.API_URL}/lounges/join/`,
+    headers: {
+      Authorization: `Token ${accountStore.token}`,
+    },
+    data: {
+      code: codeInput.value,
+    },
+  })
+    .then((res) => {
+      accountStore.getUserInfo();
+      toggleModal();
+    })
+    .catch((err) => {
+      errorMessage.value = JSON.parse(err.request.response).message;
+    });
+};
+
+const createLounge = function () {
+  axios({
+    method: "post",
+    url: `${accountStore.API_URL}/lounges/create/`,
+    headers: {
+      Authorization: `Token ${accountStore.token}`,
+    },
+    data: {
+      name: loungeName.value,
+      description: loungeDescription.value,
+    },
+  })
+    .then((res) => {
+      accountStore.getUserInfo();
+      toggleModal();
+    })
+    .catch((err) => {
+      errorMessage.value = JSON.parse(err.request.response).message;
+    });
+};
 </script>
 
 <style scoped>
